93f2068d59d074bf322c9d5c8dd42a4f24f1474b
decampsm <e.ziitron@gmail.com>
2018-05-31 13:22:46 +0200

    proto3

--- src/proto/Circle.java
+++ src/proto/Circle.java
@@ -8,6 +8,7 @@ public class Circle {
 	private int y;
 	private int diameter;
 	private long m_lastMove;
+	private int step = 8;
 
 	Circle() {
 		x = 30;
@@ -24,11 +25,25 @@ public class Circle {
 	}
 
 	public void step(long now) {
-		long elapsed = now - m_lastMove;
-		if (elapsed > 50L) {
-			x += 1;
-			m_lastMove = now;
-		}
-
+//		long elapsed = now - m_lastMove;
+//		if (elapsed > 50L) {
+//			x += 1;
+//			m_lastMove = now;
+//		}
+	}
+	
+	public void step(char direction) {
+		if (direction == 'R') {
+			  x += 3*step;
+		  }
+		  else if(direction == 'L') {
+			  x -= 3*step;
+		  }
+		  else if(direction == 'D') {
+			  y += 3*step;
+		  }
+		  else if(direction == 'U') {
+			  y -= 3*step;
+		  }
 	}
 }
--- src/proto/Controller.java
+++ src/proto/Controller.java
@@ -6,9 +6,10 @@ import java.awt.event.MouseEvent;
 import edu.ricm3.game.GameController;
 import proto.Model;
 
-public class Controller extends GameController{
+public class Controller extends GameController {
 	private Model m_model;
-	
+	boolean P1_move;
+
 	public Controller(Model m) {
 		m_model = m;
 	}
@@ -16,72 +17,87 @@ public class Controller extends GameController{
 	@Override
 	public void notifyVisible() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void step(long now) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (e.getKeyCode() == 39) {
+			P1_move = true;
+			m_model.getCircle().step('R');
+		} else if (e.getKeyCode() == 38) {
+			P1_move = true;
+			m_model.getCircle().step('U');
+		} else if (e.getKeyCode() == 37) {
+			P1_move = true;
+			m_model.getCircle().step('L');
+		} else if (e.getKeyCode() == 40) {
+			P1_move = true;
+			m_model.getCircle().step('D');
+		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		int keyCode = e.getKeyCode();
+		if(keyCode == 37 || keyCode == 38 | keyCode == 39 || keyCode == 40) {
+			P1_move = false;
+		}
+
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }

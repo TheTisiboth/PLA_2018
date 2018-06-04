5d32f0cbf73c7c26f03154980ac6d898c6fc2aa4
thetisiboth <thetisiboth@gmail.com>
2018-06-04 16:00:08 +0200

    reduction nombre obstacles

--- src/mvc/MesOptions.java
+++ src/mvc/MesOptions.java
@@ -6,7 +6,7 @@ public class MesOptions {
 	public static final int nbCol  = 24;
 	public static final int nbLigne = 12;
 	public static final int nombre_case = nbCol*nbLigne;
-	public static int nb_obstacle = 40;
+	public static int nb_obstacle = 30;
 	public static final int popBonus = 2; // 1/popBonus chances de faire pop un bonus par seconde 
 	public static final int depopBonus = 5; // temps avant depop (secondes)
 }

93cbc8ab9b038e18fefc044c48569a7c1f644f40
decampsm <e.ziitron@gmail.com>
2018-06-11 10:54:36 +0200

    sprite zbire

--- src/physic/entity/Zbire.java
+++ src/physic/entity/Zbire.java
@@ -4,6 +4,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import mvc.MesOptions;
 
@@ -11,23 +15,25 @@ public class Zbire extends Physic_Entity {
 
 	private Color couleur;
 	private int nb_case, type, joueur;
-	int m_w, m_h, m_idx = 1, m_nrows, m_ncols;
+	int m_w, m_h, m_idx, m_nrows, m_ncols;
 	float m_scale;
 	BufferedImage m_sprite;
 	BufferedImage[] m_sprites;
 	char direction;
+	private long m_lastMove;
 
-	public Zbire(BufferedImage sprite, Integer rows, Integer columns, int x, int y, Color c, int n, int type,
-			float scale, int joueur) {
+	public Zbire(int x, int y, Color c, int n, int type, float scale, int joueur) {
 		super(x, y);
-		m_sprite = sprite;
-		m_nrows = rows;
-		m_ncols = columns;
+		m_nrows = 3;
+		m_ncols = 4;
+		m_idx = 4;
 		this.setCouleur(c);
 		nb_case = n;
 		this.setType(type);
 		m_scale = scale;
 		this.joueur = joueur;
+		m_lastMove = 0;
+		loadSprite(type);
 		splitSprite();
 	}
 
@@ -54,6 +60,24 @@ public class Zbire extends Physic_Entity {
 		}
 	}
 
+	private void loadSprite(int type) {
+		File sprite = null;
+		if (type == 0)
+			sprite = new File("images/sbires_bleu.png");
+		else if (type == 1)
+			sprite = new File("images/sbires_jaune.png");
+		else if (type == 2)
+			sprite = new File("images/sbires_rose.png");
+		else if (type == 3)
+			sprite = new File("images/sbires_vert.png");
+		try {
+			m_sprite = ImageIO.read(sprite);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
 	public void paint(Graphics g) {
 		Image img = m_sprites[m_idx];
 		int w = (int) (m_scale * m_w);
@@ -62,6 +86,16 @@ public class Zbire extends Physic_Entity {
 	}
 
 	public void step(long now) {
+			if(m_lastMove == 0) {
+				m_lastMove = now - 101L;
+			}
+			long elapsed = now - m_lastMove;
+			if (elapsed > 200L) {
+				System.out.println("Le zbire doit bougé, nbcase: "+nb_case);
+				m_idx = (m_idx == 4) ? 0 : 4;
+				m_lastMove = now;
+				nb_case--;
+			}
 	}
 
 	public void reduce_nb_case() {
